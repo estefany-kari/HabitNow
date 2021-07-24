e00080d06f89c33cbe91dc951dfb65b816a6d50b
Estefany Canencia <estefany.canencia@epn.edu.ec>
2021-07-24 03:07:30 -0500

    con que frecuencia - cuando quieres hacerlo

---
+++ ProyectoG2/app/src/main/java/com/example/proyectog2/CuandoQuieresHacerlo.kt
@@ -0,0 +1,11 @@
+package com.example.proyectog2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CuandoQuieresHacerlo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cuando_quieres_hacerlo)
+    }
+}
