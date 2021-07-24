dd69f0a1c8c70d89ea9d23d314c466d421e1815b
Estefany Canencia <estefany.canencia@epn.edu.ec>
2021-07-24 03:32:02 -0500

    Cuando desea hacerlo=?

--- ProyectoG2/app/src/main/java/com/example/proyectog2/CuandoQuieresHacerlo.kt
+++ ProyectoG2/app/src/main/java/com/example/proyectog2/CuandoQuieresHacerlo.kt
@@ -2,10 +2,13 @@ package com.example.proyectog2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
 
 class CuandoQuieresHacerlo : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar()?.hide();
         setContentView(R.layout.activity_cuando_quieres_hacerlo)
     }
 }
