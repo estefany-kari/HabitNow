323bc12d1c1d0ca3adb701ff2a76f6a84bcd5f4f
CinthiaBR <cinthia.burbano@epn.edu.ec>
2021-07-25 03:27:16 -0500

    Función registrarUsuario

--- ProyectoG2/app/src/main/java/com/example/proyectog2/registrarUsuario.kt
+++ ProyectoG2/app/src/main/java/com/example/proyectog2/registrarUsuario.kt
@@ -1,5 +1,6 @@
 package com.example.proyectog2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -7,6 +8,7 @@ import android.view.Window
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_registrar_usuario.*
 
 class registrarUsuario : AppCompatActivity() {
     private lateinit var fecha: EditText
@@ -17,17 +19,28 @@ class registrarUsuario : AppCompatActivity() {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getSupportActionBar()?.hide();
         setContentView(R.layout.activity_registrar_usuario)
-
+        BaseDeDatos.TablaUsuario = SQLiteHelper(this)
+        btn_cancelar_registroUsuario.setOnClickListener {
+            val intent: Intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
         fecha = findViewById<EditText>(R.id.txtFecha)
 
         fecha.setOnClickListener{ShowDatePickerDialog()}
 
         val botonRegistrar= findViewById<Button>(R.id.btn_ingresarUsuario)
         botonRegistrar.setOnClickListener {
-            agregarNuevoUsuario()
-
-
+            if(BaseDeDatos.TablaUsuario!=null){
+                agregarNuevoUsuario()
+                //EBaseDeDatos.TablaUsuario!!.crearUsuarioFormulario(txtNombreAAgregar,txtdescAAgregar)
+                etNombre.setText("")
+                edi_nombreUsuario.setText("")
+                txtFecha.setText("")
+                editTextTextPassword.setText("")
+                editTextTextPassword2.setText("")
+            }
         }
+
     }
     fun ShowDatePickerDialog() {
         val datePicker = DateIckerFragment{ day, month, year -> OnDateSelected(day, month, year)}
@@ -39,18 +52,15 @@ class registrarUsuario : AppCompatActivity() {
     }
     fun agregarNuevoUsuario(){
 
-        val nombre= findViewById<EditText>(R.id.etNombre).toString()
-        val editNombreUsuario = findViewById<EditText>(R.id.edi_nombreUsuario).toString()
-        val fechaNac = findViewById<EditText>(R.id.txtFecha).toString()
-        val clave1= findViewById<EditText>(R.id.editTextTextPassword).toString()
+        val txtNombreAAgregar = etNombre.text.toString()
+        val txtNombreUsuario = edi_nombreUsuario.text.toString()
+        val txtfechaNac= txtFecha.text.toString()
+        val txtclave1 = editTextTextPassword.text.toString()
+        val lista = datos.crearUsuarioFormulario(txtNombreAAgregar,txtNombreUsuario,txtfechaNac,txtclave1)
 
-
-        val listaUsuario = datos.crearUsuarioFormulario(nombre,editNombreUsuario,clave1, fechaNac)
-
-
-        if(listaUsuario != null){
+        if(lista != null){
             Toast.makeText(this,"Usuario registrado correctamente", Toast.LENGTH_SHORT).show()
-            Log.i("anadir-usuario","${nombre} ---> ${editNombreUsuario}, $fechaNac, $clave1")
+            Log.i("anadir-usuario","${txtNombreAAgregar} ---> ${txtNombreUsuario}, ${txtfechaNac}, ${txtclave1}")
         }else{
             Toast.makeText(this,"Usuario no Registrado", Toast.LENGTH_SHORT).show()
 
